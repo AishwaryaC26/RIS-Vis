d7b5bd3cf55adc71f86b7d8b9b62049100f965e8
AishwaryaC26 <aishu.akku0406@gmail.com>
2023-06-20 09:11:03 -0400

    Edited files

--- app/testingapi.py
+++ app/testingapi.py
@@ -10,6 +10,8 @@ import pandas as pd
 
 import time
 import matplotlib.pyplot as plt
+import mpld3
+from obspy.signal import PPSD
 
 start = time.time()
 client = Client('IRIS')
@@ -25,14 +27,14 @@ endtime = UTCDateTime('2022-12-05T00:00:00')
 
 # query for data
 st = client.get_waveforms(net, sta, loc, chan, starttime, endtime, attach_response = True)
+inv = client.get_stations(network=net,station= sta, location=loc, channel=chan, starttime=starttime, endtime=endtime, level = "response")
 
-st.spectrogram()
-st.spectrogram(log = True, dbscale = True) #do not plot spectrogram
-
-print(time.time() - start)
 
 tr = st[0]
-
+ppsd = PPSD(tr.stats, metadata=inv)
+ppsd.add(st)
+ppsd.plot()
+'''
 tr2 = st.copy()
 tr2.filter('bandpass', freqmin=0.04, freqmax=0.07, corners=1, zerophase=True)
 tr2.plot()
@@ -59,5 +61,5 @@ print(time.time() - start)
 fig = px.line(df, x="times", y="Data", title='Waveform Data', render_mode='webgl')
 print(time.time() - start)
 
-
+'''
 #creating spectrogram
