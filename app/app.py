9fb565e62ecc7806056610f9a3518b73fb9d9868
Aishwarya Chakravarthy <achakrav@mit.edu>
2023-06-21 16:43:08 -0400

    Edited Files + Added Docker

--- app/app.py
+++ app/app.py
@@ -1,4 +1,3 @@
-
 """
 MIT Haystack REU Project: Dashboard to track Antarctic Ross Ice Shelf Data
 Built by: Aishwarya Chakravarthy
@@ -17,14 +16,20 @@ import plotly.express as px
 import datetime
 from datetime import date
 
-import mpld3     
+import mpld3    
 import matplotlib
 import matplotlib.pyplot as plt
 
 from obspy.signal import PPSD
+import base64
+
+import time
+import atexit
+
+from apscheduler.schedulers.background import BackgroundScheduler
 
 
-matplotlib.use('agg') # prevents matplotlib from plotting 
+matplotlib.use('agg') # prevents matplotlib from plotting
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 client = Client('IRIS') #establish client to access API
@@ -32,16 +37,13 @@ client = Client('IRIS') #establish client to access API
 
 ### GLOBAL VARIABLES ###
 
-startdate = date(2022, 1, 1) #change this date to reflect first date of data collection of SGIP
-# this date was chosen specifically to test this application with the ELHT station
-
 # IRIS seismic stations
-# Add new stations in the following format - 
+# Add new stations in the following format -
 # Station name: {"net": network, "loc": location, "chan": channel}
 ### NOTE: format might need to be changed if multiple stations have the same name ###
-stations = {"HOO": {"net": "1G", "loc": "--", "chan" : "LHZ"}, 
-            "CONZ": {"net": "2H", "loc": "--", "chan": "LHZ"}, 
-            "ELHT": {"net": "2H", "loc": "--", "chan": "LHZ"}, 
+stations = {"HOO": {"net": "1G", "loc": "--", "chan" : "LHZ"},
+            "CONZ": {"net": "2H", "loc": "--", "chan": "LHZ"},
+            "ELHT": {"net": "2H", "loc": "--", "chan": "LHZ"},
             "NAUS": {"net": "2H", "loc": "--", "chan": "LHZ"}}
 
 #create list of stations from "stations" dict
@@ -98,31 +100,31 @@ app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
 #all calculative methods
 
 #method to create waveform
-def create_waveform_graph(net, sta, chan, starttime, endtime, filterselect, minfreq, maxfreq, 
+def create_waveform_graph(net, sta, chan, starttime, endtime, filterselect, minfreq, maxfreq,
                           cornersinp, zerophase):
     # query for data
-    try: 
+    try:
         st = client.get_waveforms(net, sta, '--', chan, starttime, endtime, attach_response = True)
         inv = client.get_stations(network=net,station= sta, channel=chan, starttime=starttime, endtime=endtime, level = "response")
 
     except obspy.clients.fdsn.header.FDSNNoDataException as e:
         st = None
-        #inv = None
-        return [st, px.line()]
+        inv = None
+        return [st, px.line(), inv]
 
     filtered_data = apply_filter(st, filterselect, minfreq, maxfreq, cornersinp, zerophase)
 
     waveformdata = filtered_data[0].data
     waveformtimes = filtered_data[0].times(type="relative")
     waveformtimes = [tr/86400 for tr in waveformtimes]
-    
+   
     df = pd.DataFrame({
-        'Time (in days)': waveformtimes, 
+        'Time (in days)': waveformtimes,
         'Amplitude': waveformdata,
     })
 
     fig = px.line(df, x="Time (in days)", y="Amplitude",render_mode='webgl')
-    return [filtered_data, fig]
+    return [filtered_data, fig, inv]
 
 # method to apply filter to waveform
 def apply_filter(data, filtertype, minfreq, maxfreq, corners, zerophase):
@@ -132,10 +134,10 @@ def apply_filter(data, filtertype, minfreq, maxfreq, corners, zerophase):
                     corners = corners, zerophase = zerophase)
         return data
     elif filtertype == "lowpass" or filtertype == "highpass":
-        data.filter(type = filtertype, freq = minfreq, corners = corners, 
+        data.filter(type = filtertype, freq = minfreq, corners = corners,
                     zerophase = zerophase)
     return data
-    
+   
 
 def create_spectrogram(currentwave, dbscaleinp):
     #using currentwave variable- which represents the current waveform user is examining
@@ -147,10 +149,15 @@ def create_spectrogram(currentwave, dbscaleinp):
 
 #creating the PSD
 def create_psd(currentwave, inventory):
+    if not currentwave:
+        return None
     tr = currentwave[0]
-    ppsd = PPSD(tr.stats, metadata=inv)
+    ppsd = PPSD(tr.stats, metadata=inventory)
     ppsd.add(currentwave)
     ppsd.plot(show=False)
+    plt.savefig('ppsd.png')
+    image_filename = 'ppsd.png' # replace with your own image
+    return base64.b64encode(open(image_filename, 'rb').read())
 
 
 #Seismic Page Components
@@ -162,9 +169,9 @@ seismic_introduction = dbc.Container(
         html.P(
             "Use the below features to examine waveform data and generate spectograms.",
             className="lead",
-        ), 
+        ),
          html.P(
-            "It is recommended to choose time periods that do not last" + 
+            "It is recommended to choose time periods that do not last" +
             " longer than a month. If an empty graph is displayed, data is not available for the time period chosen.",
             className="lead",
         )
@@ -183,13 +190,13 @@ dropdown = html.Div(
             stationsoptions[0],
             id="dropdownseismic",
             clearable=False,
-        ), 
+        ),
     ],
     className="mb-4",
 )
 
 #Time frame picker
-datePicker = html.Div( 
+datePicker = html.Div(
     [   dbc.Row([dbc.Label("Select date range:")]),    
         dcc.DatePickerRange(
         id='timeframepicker',
@@ -199,14 +206,14 @@ datePicker = html.Div(
         start_date=date(2022, 12, 1), #date.today() - datetime.timedelta(2),
         end_date=date(2022, 12, 5) #date.today()
     )
-    ], 
+    ],
      className="mb-4",
 )
 
-#waveform filter settings 
+#waveform filter settings
 pickfilter = html.Div([
         html.Hr(),
-        dbc.Label("Waveform filters:"), 
+        dbc.Label("Waveform filters:"),
         dbc.InputGroup(
             [
                 dbc.Select(
@@ -225,7 +232,7 @@ pickfilter = html.Div([
         dbc.Label("Filter settings:"),
 
         dbc.InputGroup(
-            [dbc.InputGroupText("Minimum Frequency", id = "minfreqtext"), dbc.Input(placeholder="0.1", type = "number", id = "minfreqinput"), 
+            [dbc.InputGroupText("Minimum Frequency", id = "minfreqtext"), dbc.Input(placeholder="0.1", type = "number", id = "minfreqinput"),
             dbc.FormFeedback("Please input a value.", type="invalid",),],
             className="mb-3",
         ),
@@ -237,7 +244,7 @@ pickfilter = html.Div([
         dbc.InputGroup(
             [dbc.InputGroupText("Corners"), dbc.Input(placeholder="4", type = "number", id = "cornersinput"), dbc.FormFeedback("Please input a value.", type="invalid",),],
             className="mb-3",
-        ), 
+        ),
          dbc.InputGroup(
             [
                 dbc.InputGroupText("Zerophase"),
@@ -258,7 +265,7 @@ pickfilter = html.Div([
 )
 
 
-#callback so that filter settings only pops up when filter is chosen 
+#callback so that filter settings only pops up when filter is chosen
 @app.callback(
         Output("filtersettingsdiv", "hidden"),
         Output("maxfreq", "hidden"),
@@ -269,7 +276,7 @@ def update_filter_form(value):
     retthis = "Minimum Frequency"
     if value == "highpass" or value == "lowpass":
         retthis = "Corner Frequency"
-    return value == "None" or value == None, value == "highpass" or value == "lowpass", retthis 
+    return value == "None" or value == None, value == "highpass" or value == "lowpass", retthis
 
 spectrogramfilters = html.Div([
     html.Hr(),
@@ -283,13 +290,13 @@ spectrogramfilters = html.Div([
                         {"label": "No"},
                     ], value="No", id="dbscaleinput"
                 ),
-            ], 
+            ],
             className="mb-3",
         ),
 ])
 
 #creating a form with all of the inputs
-controls = dbc.Card([dbc.Form( [dropdown, datePicker, pickfilter, spectrogramfilters, dbc.Button("Submit", color="primary", 
+controls = dbc.Card([dbc.Form( [dropdown, datePicker, pickfilter, spectrogramfilters, dbc.Button("Submit", color="primary",
                                         className="mb-3", id = "formsubmitbut", disabled=True)])], body = True)
 
 
@@ -312,7 +319,7 @@ def check_incorrect_input(minfreq, maxfreq, corners):
     Input("minfreqinput", "value"),
     Input("maxfreqinput", "value"),
     Input("cornersinput", "value"),
-    Input("filterselect", "value"), 
+    Input("filterselect", "value"),
 )
 def make_submit_visible(minfreqinput, maxfreqinput, cornersinput, filterselect):
     if filterselect=="None":
@@ -321,7 +328,7 @@ def make_submit_visible(minfreqinput, maxfreqinput, cornersinput, filterselect):
         return not (minfreqinput and cornersinput and filterselect)
     if minfreqinput and maxfreqinput and cornersinput and filterselect:
         return False
-    return True 
+    return True
 
 
 #callback to remove all values in form if filter is changed
@@ -341,29 +348,23 @@ waveform_graph = dbc.Card([html.H3("Waveform"), dcc.Loading(
     children=[dcc.Graph(id="waveformgraph"),],
     type="circle")], body = True, )
 
-spectogram_graph = dbc.Card([html.H3("Spectrogram"), dcc.Loading(
+spectrogram_graph = dbc.Card([html.H3("Spectrogram"), dcc.Loading(
                 children = [html.Iframe(
                 id='spectrogram', srcDoc=None,  
                 style={'border-width': '5', 'width': '100%',
                        'height': '500px'})], type="circle"  
                 )], body = True, style={"width": "50%"})
 
-tabs = dbc.Card([html.H3("Waveform Data", className="display-6"), 
-                 dcc.Loading(
-                children=[dcc.Graph(id="waveformgraph"),],
-                type="circle"), 
-                
-                dcc.Loading(
-                children = [html.Iframe(
-                id='spectrogram', srcDoc=None,  
-                style={'border-width': '5', 'width': '100%',
-                       'height': '500px'})], type="circle"  
-                )], body=True,)
+psd_graph = dbc.Card([html.H3("Power Spectral Density"), dcc.Loading(
+    children=[html.Img(id = "psd")],
+    type="circle")], body = True, style={"width": "50%"})
+
 
 #callback to use date-picker and station selection to create graph
 @app.callback(
         Output("waveformgraph", "figure"),
         Output("spectrogram", "srcDoc"),
+        Output("psd", "src"),
         State("timeframepicker", "start_date"),
         State("timeframepicker", "end_date"),
         State("dropdownseismic", "value"),
@@ -375,14 +376,18 @@ tabs = dbc.Card([html.H3("Waveform Data", className="display-6"),
         State("dbscaleinput", "value"),
         Input("formsubmitbut", "n_clicks")
 )
-def update_seismometer_graph(start_date, end_date, dropdownvalue, 
+def update_seismometer_graph(start_date, end_date, dropdownvalue,
                              filterselect, minfreq, maxfreq, cornersinp, zerophase,
                              dbscaleinp, n_clicks):
     start_date = UTCDateTime(start_date + 'T00:00:00')
     end_date = UTCDateTime(end_date + 'T00:00:00')
-    results = create_waveform_graph(stations[dropdownvalue]["net"], dropdownvalue, stations[dropdownvalue]["chan"], start_date, end_date, 
+    results = create_waveform_graph(stations[dropdownvalue]["net"], dropdownvalue, stations[dropdownvalue]["chan"], start_date, end_date,
                                     filterselect, minfreq, maxfreq, cornersinp, zerophase)
-    return results[1], create_spectrogram(results[0], dbscaleinp)
+    spectrogram = create_spectrogram(results[0], dbscaleinp)
+    psd = create_psd(results[0], results[2])
+    if not psd:
+        return results[1], spectrogram, None
+    return results[1], spectrogram, 'data:image/png;base64,{}'.format((psd).decode())
 
 
 #page callback- establishes different pages of website
@@ -393,11 +398,12 @@ def render_page_content(pathname):
     elif pathname == "/seismic":
         return html.Div(children=[
             seismic_introduction,
-            controls, 
-            html.Br(), 
+            controls,
+            html.Br(),
             waveform_graph,
-            html.Br(), 
-            spectogram_graph,
+            html.Br(),
+            dbc.Row([spectrogram_graph,
+                     psd_graph])
         ])
     elif pathname == "/gps":
         return html.P("Oh cool, this is page 2!")
@@ -448,14 +454,14 @@ st_flt = st.copy()
 st_flt = st_flt.filter('bandpass', freqmin = 0.001, freqmax = 0.1)
 
 st_disp = st_flt.copy()
-#st_disp = st_disp.remove_response(output = "DISP") 
+#st_disp = st_disp.remove_response(output = "DISP")
 
 tr = st_disp[0]
 waveformdata = tr.data
 waveformtimes = tr.times(type="utcdatetime")
 
 df = pd.DataFrame({
-    'times': waveformtimes, 
+    'times': waveformtimes,
     'Data': waveformdata
 })
 
--- app/testingapi.py
+++ app/testingapi.py
@@ -12,7 +12,15 @@ import time
 import matplotlib.pyplot as plt
 import mpld3
 from obspy.signal import PPSD
+from skimage import io
 
+import plotly.express as px
+from skimage import io
+img = io.imread('ppsd.png')
+fig = px.imshow(img)
+fig.show()
+
+'''
 start = time.time()
 client = Client('IRIS')
 net = '2H'
@@ -23,17 +31,18 @@ chan = 'LHZ'
 #setting start time and end time
 starttime = UTCDateTime('2022-12-01T00:00:00')
 endtime = UTCDateTime('2022-12-05T00:00:00')
-
+img = io.imread('ppsd.png')
+print(img)
 
 # query for data
 st = client.get_waveforms(net, sta, loc, chan, starttime, endtime, attach_response = True)
 inv = client.get_stations(network=net,station= sta, location=loc, channel=chan, starttime=starttime, endtime=endtime, level = "response")
 
-
 tr = st[0]
 ppsd = PPSD(tr.stats, metadata=inv)
 ppsd.add(st)
-ppsd.plot()
+ppsd.plot(show=False)
+plt.savefig('ppsd.png') '''
 '''
 tr2 = st.copy()
 tr2.filter('bandpass', freqmin=0.04, freqmax=0.07, corners=1, zerophase=True)
