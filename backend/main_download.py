2b528885647c64d511e3400adc2e96eff0bb8cbd
Aishwarya Chakravarthy <achakrav@mit.edu>
2023-06-22 19:23:32 -0400

    Edited files

--- backend/main_download.py
+++ backend/main_download.py
@@ -2,14 +2,16 @@ import os
 import logging
 from datetime import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
-from obspy.clients.fdsn import Client
-from IRIS_seismic_data.helpers.seismic_data_pull import 
+from IRIS_seismic_data.helpers.seismic_data_pull import nightly_download_mseed_waveform
 
-def get_data(cl):
-    
+
+
+def get_data():
+    nightly_download_mseed_waveform()
+
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, filename="./logs/main.log", format='%(asctime)s:%(levelname)s:%(message)s')
+    #logging.basicConfig(level=logging.INFO, filename="./logs/main.log", format='%(asctime)s:%(levelname)s:%(message)s')
     scheduler = BlockingScheduler()
-    scheduler.add_job(get_data(), 'cron', minute='00', hour='03', day='*', year='*', month='*')
+    scheduler.add_job(get_data, 'cron', minute='00', hour='03', day='*', year='*', month='*')
     scheduler.start()
